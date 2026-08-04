9e1f1a632de636b9af41504b1916596a31e89ac3
Joseph TAMO <tingangt@gmail.com>
2026-08-03 23:38:01 -0400

    :tada: First commit

---
+++ src/main/java/com/jo/dev/pharmacash_backend/PharmacashBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jo.dev.pharmacash_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacashBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacashBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jo/dev/pharmacash_backend/PharmacashBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jo.dev.pharmacash_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacashBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
