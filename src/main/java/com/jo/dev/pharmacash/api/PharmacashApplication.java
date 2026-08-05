d4115fe6a4bc3ca76179699f356f68b0a42e4ca0
Joseph TAMO <tingangt@gmail.com>
2026-08-05 11:42:32 -0400

    :art: Define packages & project structure

--- src/main/java/com/jo/dev/pharmacash/api/PharmacashApplication.java
+++ src/main/java/com/jo/dev/pharmacash/api/PharmacashApplication.java
@@ -1,4 +1,4 @@
-package com.jo.dev.pharmacash;
+package com.jo.dev.pharmacash.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PharmacashApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PharmacashApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(PharmacashApplication.class, args);
+    }
 
 }
--- src/test/java/com/jo/dev/pharmacash/api/PharmacashApplicationTests.java
+++ src/test/java/com/jo/dev/pharmacash/api/PharmacashApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jo.dev.pharmacash;
+package com.jo.dev.pharmacash.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
